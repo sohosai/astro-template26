7faf662c581887d8932c2ceaa11c216d34474fb1
pocopota <pocopota.program@gmail.com>
2026-03-11 03:04:20 +0900

    stylelint整備

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('stylelint').Config} */
 export default {
-	extends: ["stylelint-config-recommended-scss"],
+	extends: [
+		"stylelint-config-recommended-scss",
+		"stylelint-config-recess-order",
+		"stylelint-config-html/astro",
+	],
+	plugins: ["stylelint-order"],
+	rules: {
+    "plugin/declaration-block-no-ignored-properties": true,
+    // RSCSS記法に適合しないためOFF
+    "no-descending-specificity": null,
+    // Astroの:globalを除外
+    "selector-pseudo-class-no-unknown": [
+      true,
+      {
+        ignorePseudoClasses: ["global"],
+      },
+    ],
+  },
+	ignoreFiles: ["./src/styles/reset.css"],
 };
